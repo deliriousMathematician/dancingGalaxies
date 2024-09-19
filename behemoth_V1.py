c1ce5bb41f2f62d05c03b9f63dccfb5a8c0560cf
Karl <179724112+Karl-Caruana@users.noreply.github.com>
2024-09-19 20:05:52 +0200

    Create behemoth_V1.py
    WORK IN PROGRESS

---
+++ behemoth_V1.py
@@ -0,0 +1,7 @@
+# I'm working on a completely different set of code, hopefully gives me a new perspective. Will upload here soon. When this is done;
+
+# IT IS TO BE MERGED WITH WHATEVER THE LATEST VERSION OF MAIN IS.
+# I decided to take an approach more from scratch so as not to cause unecessary convolusion. Hopefully it makes it easier to understand the train of thought.
+# Regardless should be easily mergable into main.
+
+# I am trying to add some novel features though, like a progress bar.
