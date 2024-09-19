323b440f07b0a783b03862365118758c4d490c58
Karl <179724112+Karl-Caruana@users.noreply.github.com>
2024-09-19 20:06:58 +0200

    Update behemoth_V1.py

--- behemoth_V1.py
+++ behemoth_V1.py
@@ -1,4 +1,4 @@
-# I'm working on a completely different set of code, hopefully gives me a new perspective. Will upload here soon. When this is done;
+# I'm working on a completely different code set since we still need to merge the 3 different main versions. The side benefit might be that it gives me a new perspective. Will upload here soon. When this is done;
 
 # IT IS TO BE MERGED WITH WHATEVER THE LATEST VERSION OF MAIN IS.
 # I decided to take an approach more from scratch so as not to cause unecessary convolusion. Hopefully it makes it easier to understand the train of thought.
